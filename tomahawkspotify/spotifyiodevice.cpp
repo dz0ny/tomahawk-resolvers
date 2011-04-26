0b0c0d477246a6241547be429670f6a432f621d9
Leo Franchi <lfranchi@kde.org>
2011-04-26 06:49:37 -0400

    non-working wav header

--- tomahawkspotify/spotifyiodevice.cpp
+++ tomahawkspotify/spotifyiodevice.cpp
@@ -39,11 +39,42 @@ SpotifyIODevice::~SpotifyIODevice()
         close();
 }
 
+void SpotifyIODevice::setDurationMSec( uint msec )
+{
+    uint numSamples = ( msec * 44100 ) / 1000;
+    uint dataChunkSize = numSamples * 2 * 2;
+    // got samples, we can make the header now
+    m_header += (const char* )"RIFF";
+    m_header.append((char*)(36 + dataChunkSize), 4); // The file size LESS the size of the "RIFF" description (4 bytes) and the size of file description (4 bytes). This is usually file size - 8, or 36 + subchunk2 size
+    m_header += (const char* )"WAVE"; //The ascii text string "RIFF". mmsystem.h provides the macro FOURCC_RIFF for this purpose.
+    /// fmt subchunk
+    m_header += (const char* )"fmt "; //
+    m_header.append((char*)(quint32)16, 4); // The size of the WAVE type format (2 bytes) + mono/stereo flag (2 bytes) + sample rate (4 bytes) + bytes/sec (4 bytes) + block alignment (2 bytes) + bits/sample (2 bytes). This is usually 16 (or 0x10).
+    m_header .append((char*)1, 2); // Type of WAVE format. This is a PCM header, or a value of 0x01.
+    m_header.append((char*)2, 2); // mono (0x01) or stereo (0x02)
+    m_header.append((char*)44100, 4); // Sample rate.
+    m_header.append((char*)(44100 * 2 * 16/8), 4); // Bytes/Second == SampleRate * NumChannels * BitsPerSample/8
+    m_header.append((char*)(2 * 16/8) , 2); // Data block size (bytes) == NumChannels * BitsPerSample/8
+    m_header.append((char*)16, 2); // bits per sample
+    /// data subchunk
+    m_header += "data" ;
+    m_header.append((char*)dataChunkSize, 4); // NumSamples * NumChannels * BitsPerSample/8
+}
+
+
 qint64 SpotifyIODevice::readData( char* data, qint64 maxlen )
 {
     QMutexLocker l( &m_mutex );
 
     qint64 written = 0;
+
+    if( !m_header.isEmpty() && m_header.size() < maxlen ) {
+        qMemCopy( data, m_header.constData(), m_header.size() );
+        qDebug() << "wrote header:" << m_header.toHex();
+        written += m_header.size();
+
+        m_header.clear();
+    }
 
     while( !m_audioData.isEmpty() && written < maxlen ) {
         qint64 next = m_audioData.first().second;
@@ -81,7 +112,7 @@ qint64 SpotifyIODevice::writeData( const char* data, qint64 len )
 
 qint64 SpotifyIODevice::bytesAvailable() const
 {
-    return m_curSum;
+    return m_header.size() + m_curSum;
 }
 
 void SpotifyIODevice::disconnected()
